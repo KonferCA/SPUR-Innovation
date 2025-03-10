691a2daf7890011fd207ab754ab4d146878848c2
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-03-10 19:44:18 -0400

    [add] spur logo v2

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,9 +4,10 @@
  *
  */
 import SpurLogo from "./spurLogo.svg";
+import SpurLogoV2 from "./spurLogoV2.svg";
 import SpurBuilding from "./spurBuilding.png";
 import xLogo from "./xLogo.svg";
 import HalfEllipse from "./halfEllipse.svg";
 import Vexacool from "./Vexacool.png";
 
-export { SpurLogo, SpurBuilding, xLogo, HalfEllipse, Vexacool };
+export { SpurLogo, SpurLogoV2, SpurBuilding, xLogo, HalfEllipse, Vexacool };
