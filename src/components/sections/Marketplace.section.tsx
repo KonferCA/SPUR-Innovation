e379f9d20b1bb1b65709bf78e9f29685e8047d3e
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-03-27 20:26:30 -0400

    [wip] making marketplace section mobile responsive

--- src/components/sections/Marketplace.section.tsx
+++ src/components/sections/Marketplace.section.tsx
@@ -3,14 +3,21 @@ import { Button, CardComponent, Header } from "@components";
 const MarketplaceSection: React.FC = () => {
     return (
         <div>
-            <section className="relative w-full h-screen bg-gradient-to-tr from-[#F59047]/70 via-[#2C2C2F]/40 to-[#0B1219] flex justify-center items-center">
-                <div className="flex flex-col w-1/2 gap-32 p-42">
+            <section className="relative w-full h-screen bg-gradient-to-tr from-[#F59047]/70 via-[#2C2C2F]/40 to-[#0B1219] flex flex-col md:flex-row justify-center items-center">
+                <div className="flex flex-col justify-between sm:gap-20 lg:gap-32 md:p-42">
                     <div>
-                        <Header size="h1" gradient="right">
+                        <Header
+                            size="h1"
+                            gradient="right"
+                            className="flex justify-center md:justify-start"
+                        >
                             Onboard by SPUR
                         </Header>
-
-                        <Header size="h1" gradient="left">
+                        <Header
+                            size="h1"
+                            gradient="left"
+                            className="flex justify-center md:justify-start"
+                        >
                             Featured Projects
                         </Header>
                         <p className="my-10">
@@ -34,7 +41,7 @@ const MarketplaceSection: React.FC = () => {
                     </div>
                 </div>
                 {/* Card slide deck */}
-                <div className="relative w-1/2">
+                <div className="relative w-full md:mr-30">
                     <CardComponent />
                 </div>
             </section>
