99aba5445e6357026c841bb58cab37bdf0fa485d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-04-02 19:44:38 -0400

    [update] make footer responsive

--- src/components/sections/Footer.section.tsx
+++ src/components/sections/Footer.section.tsx
@@ -5,14 +5,18 @@ import { InstagramLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 const FooterSection: React.FC = () => {
     return (
         <div>
-            <section className="relative bg-[#050B10] to-10% px-28 py-16">
-                <div className="flex justify-between">
+            <section className="relative bg-[#050B10] to-10% px-10 xl:px-28 py-16">
+                <div className="flex flex-col gap-10 xl:gap-0 xl:flex-row justify-center xl:justify-between items-center xl:items-start">
                     {/* Logo */}
-                    <img src={SpurLogoV2} className="w-1/3" />
+                    <img src={SpurLogoV2} className="w-1/2 xl:w-1/3" />
                     {/* Directory */}
-                    <div className="flex gap-10 w-1/3 justify-center">
+                    <div className="flex gap-10 w-1/3 justify-center text-center xl:text-start">
                         <div className="flex flex-col gap-2">
-                            <Header size="h4" gradient="right" className="mb-2">
+                            <Header
+                                size="h4"
+                                gradient="right"
+                                className="mb-2 ml-4 xl:ml-0"
+                            >
                                 About
                             </Header>
                             <a href="/coming-soon">Our Story</a>
@@ -21,7 +25,11 @@ const FooterSection: React.FC = () => {
                             <a href="/coming-soon">Partners</a>
                         </div>
                         <div className="flex flex-col gap-2">
-                            <Header size="h4" gradient="right" className="mb-2">
+                            <Header
+                                size="h4"
+                                gradient="right"
+                                className="mb-2 ml-4 xl:ml-0"
+                            >
                                 Contact
                             </Header>
                             <a href="/coming-soon">Join SPUR</a>
@@ -30,16 +38,16 @@ const FooterSection: React.FC = () => {
                         </div>
                     </div>
                     {/* Mailing List */}
-                    <div className="flex flex-col gap-5 w-1/3 max-w-lg">
+                    <div className="flex text-center sm:text-start flex-col gap-5 xl:w-1/3">
                         <p className="text-2xl">
                             Join our mailing list for exclusive news
                         </p>
-                        <div className="flex w-full gap-8">
+                        <div className="flex justify-center sm:justify-start flex-wrap w-full gap-4">
                             <input
                                 type="text"
                                 id="fname"
                                 name="fname"
-                                defaultValue="Enter you email"
+                                defaultValue="Enter your email"
                                 className="grow pl-6 py-3 text-gray-300/60 border border-amber-600 rounded-full"
                             />
                             <Button intent="secondary">Email Me</Button>
@@ -47,21 +55,26 @@ const FooterSection: React.FC = () => {
                     </div>
                 </div>
                 {/* Logo & Copyright statement */}
-                <div className="flex justify-between pt-24">
+                <div className="flex flex-col gap-10 xl:gap-0 xl:flex-row justify-center xl:justify-between items-center xl:items-start pt-24">
                     <div className="flex gap-5 items-end">
                         <LinkedInLogoIcon
                             width={32}
                             height={32}
                             color="#969CA1"
+                            className="hover:cursor-pointer hover:scale-110"
                         />
                         <InstagramLogoIcon
                             width={32}
                             height={32}
                             color="#969CA1"
+                            className="hover:cursor-pointer hover:scale-110"
                         />
-                        <img src={xLogo} className="w-8" />
+                        <img
+                            src={xLogo}
+                            className="w-8 hover:cursor-pointer hover:scale-110"
+                        />
                     </div>
-                    <div className="flex gap-16">
+                    <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:items-start gap-4 lg:gap-16 text-center xl:text-start">
                         <p>
                             © 2025 SPUR Innovation. Konfer Inc. All rights
                             reserved
