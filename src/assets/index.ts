8d04a3b605ebc7f3e76ae9b26c9bedb5c50a6139
Nausher Rao <sherrao15@gmail.com>
2025-04-15 17:52:46 -0400

    Update asset imports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,15 +3,13 @@
  * @asset
  *
  */
-import SpurLogo from "./spurLogo.svg";
-import SpurLogoV2 from "./spurLogoV2.svg";
-import SpurBuilding from "./spurBuilding.png";
+import SpurIcon from "./spur_icon.svg";
+import SpurTitle from "./spur_title.svg";
 import SpurCampus from "./spur_campus.png";
 import xLogo from "./xLogo.svg";
 import HalfEllipse from "./halfEllipse.svg";
-import Vexacool from "./Vexacool.png";
-
-export { SpurLogo, SpurLogoV2, SpurBuilding, xLogo, HalfEllipse, Vexacool, SpurCampus };
+import Vexacool from "./placeholder.png";
+export { SpurIcon, SpurTitle, xLogo, HalfEllipse, Vexacool, SpurCampus };
 
 /*
  *
@@ -24,12 +22,24 @@ import TMU_logo from "./partners/TMU_logo.png";
 import UBC_logo from "./partners/UBC_logo.png";
 import UW_logo from "./partners/UW_logo.png";
 import WLU_logo from "./partners/WLU_logo.png";
-
 export { McGill_logo, Queens_logo, TMU_logo, UBC_logo, UW_logo, WLU_logo };
 
+/*
+ *
+ * @asset events
+ *
+ */
 import SpurHacksBanner from "./events/spurhacks_banner.svg";
 import HawkHacksBanner from "./events/hawkhacks_banner.png";
 import CUCAIBanner from "./events/cucai_banner.png";
 import NVIDIA_GTC_Banner from "./events/nvidia_gtc_banner.jpg";
-
 export { SpurHacksBanner, HawkHacksBanner, CUCAIBanner, NVIDIA_GTC_Banner }
+
+/*
+ *
+ * @asset startups
+ *
+ */
+import KonferLogo from "./startups/konfer_logo.svg";
+import RedBlueQuantumLogo from "./startups/rbq_logo.png";
+export { KonferLogo, RedBlueQuantumLogo };
